506f61799fa2e4e1b3d6e11a10fb6c105af475ae
MoseoIA <trackingentity@gmail.com>
2025-07-02 16:51:24 -0600

    Actualizar outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 # --- outputs.tf en Mod_AzDatabricks_Native ---
+# Este archivo define los valores que el módulo devuelve después de su ejecución.
 
 output "databricks_workspace_id" {
   value       = azurerm_databricks_workspace.databricks_ws.id
@@ -14,3 +15,9 @@ output "private_endpoint_id" {
   value       = azurerm_private_endpoint.databricks_pe.id
   description = "El ID del recurso del Private Endpoint creado para Databricks."
 }
+
+output "storage_account_id" {
+  value       = var.create_storage_account ? azurerm_storage_account.storage_for_databricks[0].id : null
+  description = "El ID de la cuenta de almacenamiento creada para Databricks. Será nulo si no se creó."
+}
+
